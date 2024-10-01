854b60dec2b3d1d1442a95fb2f032d37715087e9
TopScrech <sergei_saliukov@icloud.com>
2024-10-01 14:58:46 +0200

    Initial Commit

---
+++ Intent Handler/Intent_HandlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intent_HandlerApp.swift
+//  Intent Handler
+//
+//  Created by Sergei Saliukov on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Intent_HandlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
